d79fd3e2c7c89b6ed27f38ad8a89e06c8af079e4
Lorena <lorena.perez@gmail.com>
2021-06-07 16:46:42 +0200

    chage intermedia

--- Modulo 3/3-6/Intermedia/src/components/App.js
+++ Modulo 3/3-6/Intermedia/src/components/App.js
@@ -1,8 +1,9 @@
 import React from "react";
+import data from "../data/data.json";
 import "../stylesheets/App.scss";
 import ClubList from "./ClubList";
 
 function App() {
-  return <ClubList />;
+  return <ClubList list={data} />;
 }
 export default App;
--- Modulo 3/3-6/Intermedia/src/components/ClubList.js
+++ Modulo 3/3-6/Intermedia/src/components/ClubList.js
@@ -1,136 +1,14 @@
 import React from "react";
+import Club from "./Club";
 
-function ClubList() {
-  const clubs = [
-    {
-      name: "Book club",
-      fa: "fas fa-glasses",
-      members: [
-        "Rosalie Bradley",
-        "Lula Day",
-        "Hallie Bryant",
-        "Antonio Martin",
-        "Polly Nelson",
-      ],
-    },
-    {
-      name: "Chess club",
-      fa: "fas fa-chess",
-      members: [
-        "Francisco Alexander",
-        "Alice Garner",
-        "Michael Elliott",
-        "Tyler Sparks",
-        "Rose Munoz",
-        "Lena Rios",
-        "Abbie Perkins",
-      ],
-    },
-    {
-      name: "Escape room club",
-      fa: "fas fa-dungeon",
-      members: [
-        "Della Frank",
-        "Nathan Briggs",
-        "Alexander Caldwell",
-        "John McCarthy",
-        "Theodore Lawson",
-      ],
-    },
-    {
-      name: "Thief club",
-      fa: "fas fa-mask",
-      members: [
-        "Ina Becker",
-        "Jared Bryan",
-        "Eugenia Crawford",
-        "Mina Goodwin",
-        "Hester Rodriquez",
-        "Cameron Watts",
-        "Charles Daniel",
-        "Christine Barnes",
-      ],
-    },
-    {
-      name: "Fight club",
-      fa: "fas fa-fist-raised",
-      members: [
-        "Bernice Marshall",
-        "Alvin McCormick",
-        "Aiden Edwards",
-        "Daniel Mendoza",
-        "Olive Poole",
-      ],
-    },
-    {
-      name: "Magic club",
-      fa: "fas fa-magic",
-      members: [
-        "Harry Ruiz",
-        "Tom Dunn",
-        "Emily Gonzales",
-        "Evelyn Snyder",
-        "Evan Doyle",
-        "Stanley Mann",
-        "Tom Nash",
-        "Glenn Luna",
-        "Hattie McCoy",
-        "Erik Cobb",
-        "Ada Warren",
-        "Lucy Webb",
-      ],
-    },
-    {
-      name: "Live long and prosper club",
-      fa: "fas fa-hand-spock",
-      members: [
-        "Shane Lambert",
-        "Willie Young",
-        "Jane Hunt",
-        "Martha Houston",
-        "Clyde Johnston",
-        "Jonathan Brooks",
-        "Emily Howard",
-        "Steven Peters",
-        "Helen Stewart",
-        "John Cox",
-        "Lillie Moore",
-        "Chris Walters",
-        "Sally Cunningham",
-        "Ada Klein",
-        "Fanny Kelly",
-        "Jane Norton",
-        "Bertha Francis",
-        "Jane Scott",
-        "Etta Klein",
-        "Bobby Rodriguez",
-        "Tyler Goodman",
-        "Peter Ferguson",
-        "Theresa Hudson",
-        "Lloyd Soto",
-        "Stanley Hardy",
-        "Bobby Stewart",
-      ],
-    },
-  ];
-
-  return (
-    <div className="box" key={name}>
-      {clubs.map((club) => {
-        return (
-          <li className="box__card">
-            <i className={`box__card-icon ${club.fa}`}></i>
-
-            <h2>{club.name}</h2>
-            <ul>
-              <h3>Members</h3>
-              <li>{club.members}</li>
-            </ul>
-          </li>
-        );
-      })}
-    </div>
-  );
-}
-
+const ClubList = (props) => {
+  const clubItems = props.list.map((item, index) => {
+    return (
+      <li>
+        <Club club={item} />
+      </li>
+    );
+  });
+  return <ul></ul>;
+};
 export default ClubList;
