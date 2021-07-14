d852adf278ff57bab4f88d8dfa16a40eacc69a44
Lorena <lorena.perez@gmail.com>
2021-07-14 20:40:32 +0200

    change ex-3

--- Modulo 2/2-2/Exercise-4/main.js
+++ Modulo 2/2-2/Exercise-4/main.js
@@ -1,7 +1,8 @@
-'use strict';
+"use strict";
 
 const persons = 9;
 const bill = 128;
-const billPerson = (bill - 2)/persons;
+const billPerson = (bill - 2) / persons;
 console.log(billPerson);
-
+const anaBill = billPerson + 2;
+console.log(anaBill);
