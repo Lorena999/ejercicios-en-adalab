23306f48237ec9bf870a592af52c62b2477df50d
Lorena <lorena.perez@gmail.com>
2021-07-14 21:04:08 +0200

    complete exerc-7

--- Modulo 2/2-2/Exercise-6/main.js
+++ Modulo 2/2-2/Exercise-6/main.js
@@ -1,7 +1,7 @@
-'use strict'
+"use strict";
 
-const pairProgramme = 'Patricia';
-const greetingAdalabers = 'Hola ' + pairProgramme + ", encantada de conocerte.";
-console.log(greetingAdalabers);
+const pairProgramme = "Lorena";
+const greetingAdalabers = "Hola " + pairProgramme + ", encantada de conocerte.";
 
-
+const greetingAdalabers2 = `Hola ${pairProgramme} encantada de conocerte`;
+console.log(greetingAdalabers2);
