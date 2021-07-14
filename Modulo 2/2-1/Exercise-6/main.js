acf8132cc0e286dd1c9dd539cdb8a438bb75dfe2
Lorena <lorena.perez@gmail.com>
2021-07-14 20:33:01 +0200

    add exercise-3

--- Modulo 2/2-1/Exercise-6/main.js
+++ Modulo 2/2-1/Exercise-6/main.js
@@ -1,4 +1,5 @@
-'use strict';
-const boxElement = document.querySelector('.box');
-const boxContent = '<h1>Lorem ipsum</h1><img src="http://via.placeholder.com/350x150"><p>Lorem ipsum</p>';
+"use strict";
+const boxElement = document.querySelector(".box");
+const boxContent =
+  '<h1>Lorem ipsum</h1><img src="http://via.placeholder.com/350x150"><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit</p>';
 boxElement.innerHTML = boxContent;
