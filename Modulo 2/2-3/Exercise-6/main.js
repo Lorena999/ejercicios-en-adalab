8cf2a974978d3b4e33c9aee35e4b9aaf02bdbdbf
Lorena <lorena.perez@gmail.com>
2021-07-15 11:42:41 +0200

    check exerc-2-3-6

--- Modulo 2/2-3/Exercise-6/main.js
+++ Modulo 2/2-3/Exercise-6/main.js
@@ -1,14 +1,15 @@
-'use strict'
+"use strict";
 
 let avocados;
 const avocadoPrice = 1.5;
-const money = 50;
+const money = 100;
 
-const avocados = money / avocadoPrice !=== 0 ? 
-
+const avocadosNumber =
+  money >= avocadoPrice ? (avocados = money / avocadoPrice) : (avocados = 0);
+console.log(avocadosNumber);
 
 /*if (money >= avocadoPrice) {
   avocados = money / avocadoPrice;
 } else {
   avocados = 0;
-}
+}*/
