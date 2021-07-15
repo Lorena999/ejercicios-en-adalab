0ca6d5b23fa102285f08d24c7b834fd1727a0b1b
Lorena <lorena.perez@gmail.com>
2021-07-15 11:48:41 +0200

    check exerc-2-3-6

--- Modulo 2/2-3/Exercise-6/main.js
+++ Modulo 2/2-3/Exercise-6/main.js
@@ -2,11 +2,14 @@
 
 let avocados;
 const avocadoPrice = 1.5;
-const money = 100;
+const money = 1;
 
 const avocadosNumber =
   money >= avocadoPrice ? (avocados = money / avocadoPrice) : (avocados = 0);
 console.log(avocadosNumber);
+
+money >= avocadoPrice ? (avocados = money / avocadoPrice) : 0;
+console.log(avocados);
 
 /*if (money >= avocadoPrice) {
   avocados = money / avocadoPrice;
