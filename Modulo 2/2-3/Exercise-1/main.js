893385e86c9a546d0b4d7a57a49ba70af4f2dfd2
Lorena <lorena.perez@gmail.com>
2021-07-15 10:33:54 +0200

    revision exerc-1

--- Modulo 2/2-3/Exercise-1/main.js
+++ Modulo 2/2-3/Exercise-1/main.js
@@ -1,20 +1,13 @@
-'use strict';
+"use strict";
 
 // avatar por defecto
-const DEFAULT_AVATAR = 'http://placehold.it/300x300';
+const DEFAULT_AVATAR = "http://placehold.it/300x300";
 // avatar que eligió el usuario al registrarse
-let userAvatar = 'http://www.fillmurray.com/300/300';
+let userAvatar = "http://www.fillmurray.com/300/300";
 
 //userAvatar = 'http://www.fillmurray.com/300/300';
-const userImg = document.querySelector('.user__avatar');
+const userImg = document.querySelector(".user__avatar");
 userImg.src = userAvatar;
 //userImg.src =  `${userAvatar || DEFAULT_AVATAR}`;
 
-
 userImg.src = `${userAvatar || DEFAULT_AVATAR}`;
-
-
-
-
-
-
