6b570df4b8b53b16820f7edfadc8b57d594e8532
Lorena <lorena.perez@gmail.com>
2021-07-15 11:15:04 +0200

    check exerc-2-3

--- Modulo 2/2-3/Exercise-4/main.js
+++ Modulo 2/2-3/Exercise-4/main.js
@@ -1,15 +1,18 @@
-const ageDog = 4;
-const ageDog2 = 24 + (ageDog - 2) * 5
-/*const ageHuman = 15;*/
+let ageDog = 9;
+const ageHuman2 = 24 + (ageDog - 2) * 5;
 
 if (ageDog === 1) {
   ageHuman = 15;
-  console.log('La edad de tu perro es de ' + ageDog + ' año, lo que equivale a ' + ageHuman + ' años de humano');
+  console.log(
+    `La edad de tu perro es de ${ageDog} año, lo que equivale a ${ageHuman} años de humano.`
+  );
 } else if (ageDog === 2) {
   ageHuman = 24;
-  console.log('Tu perro tiene ' + ageDog + 'años, que equivalen a' + ageHuman + ' de humano');
+  console.log(
+    `Tu perro tiene ${ageDog} años, que equivalen a ${ageHuman} años de humano.`
+  );
 } else if (ageDog >= 3) {
-  console.log('Tu perro tiene ' + ageDog + 'años, lo que equivale a ' + ageDog2 + 'humanos'); }
-
-  
-
+  console.log(
+    `Tu perro tiene ${ageDog} años, que equivalen a ${ageHuman2} años de humano.`
+  );
+}
