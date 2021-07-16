186e952e17cd0f88d4c890ae20c86696e9973124
Lorena <lorena.perez@gmail.com>
2021-07-16 11:26:57 +0200

    check exerc-2-4-5

--- Modulo 2/2-4/Exercise-5/main.js
+++ Modulo 2/2-4/Exercise-5/main.js
@@ -10,3 +10,14 @@ console.log(btnEl);
 
 const title = getEl(".js-title");
 console.log(title);
+
+function getEl2(sel) {
+  const selector = document.querySelector(sel);
+  if (selector === null) {
+    return `No existe ningún elemento con clase, id o tag llamado ${sel}`;
+  } else {
+    return document.querySelector(sel);
+  }
+}
+const text2 = getEl2(".jsr-text");
+console.log(text2);
