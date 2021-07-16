f3ea11981d018d446af8f973169bd5a870ae6698
Lorena <lorena.perez@gmail.com>
2021-07-16 12:24:51 +0200

    check exerc-2-4-9

--- Modulo 2/2-4/Exercise-9/main.js
+++ Modulo 2/2-4/Exercise-9/main.js
@@ -1,6 +1,6 @@
 "use strict";
+//practise arrow functions
 
-"use strict";
 // modificamos una variable de ámbito global
 /*let secretLetter = "y";
 function mySecretLetter() {
@@ -16,3 +16,36 @@ const mySecretLetter = () => {
 };
 console.log(secretLetter);
 console.log(mySecretLetter());
+
+/*function mult(a, b) {
+  return a * b;
+}*/
+
+// arrow fuction
+const mult = (a, b) => a * b;
+
+let multResult = mult(6, 3);
+console.log(multResult);
+
+/*function media(a, b, c, d) {
+  return (a + b + c + d) / 4;
+}*/
+// arrow fuction
+const media = (a, b, c, d) => a + b + c + d;
+let mediaResult = media(2, 3, 4, 5);
+console.log(mediaResult);
+
+/*
+function taxIva (price) {
+  const iva = price * 0.21;
+  const priceTotal = price + iva;
+  return `El precio sin IVA es: ${price} , el IVA es: ${iva}, y el total es: ${priceTotal}`;
+};*/
+
+// arrow fuction
+const price = 1000;
+const taxIVA = (price) => {
+  const iva = price * 0.21;
+  const priceTotal = price + iva;
+  return `El precio sin IVA es: ${price} , el IVA es: ${iva}, y el total es: ${priceTotal}`;
+};
