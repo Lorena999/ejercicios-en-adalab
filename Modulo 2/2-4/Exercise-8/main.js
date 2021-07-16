0f4f60eeebb6ab137c8c9120a26927216eb2a979
Lorena <lorena.perez@gmail.com>
2021-07-16 11:44:07 +0200

    check exerc-2-4-8

--- Modulo 2/2-4/Exercise-8/main.js
+++ Modulo 2/2-4/Exercise-8/main.js
@@ -1,3 +1,15 @@
 "use strict";
 
 "use strict";
+// modificamos una variable de ámbito global
+let secretLetter = "y";
+function mySecretLetter() {
+  secretLetter = "x";
+  return secretLetter;
+}
+
+//console.log(mySecretLetter()); // devuelve "x"
+//console.log(secretLetter); // devuelve "x"
+
+console.log(secretLetter); // devuelve "y"
+console.log(mySecretLetter()); // devuelve "x"
