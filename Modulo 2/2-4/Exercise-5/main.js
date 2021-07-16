b96fcea411c25595b784822821148431b6b4fb79
Lorena <lorena.perez@gmail.com>
2021-07-16 11:10:08 +0200

    check exerc-2-4-5

--- Modulo 2/2-4/Exercise-5/main.js
+++ Modulo 2/2-4/Exercise-5/main.js
@@ -1,8 +1,12 @@
-'use strict'
-function getEl(sel){
-return document.querySelector(sel); 
+"use strict";
+function getEl(sel) {
+  return document.querySelector(sel);
 }
-const text = getEl('.js-text');
+const text = getEl(".js-text");
 console.log(text);
 
+const btnEl = getEl(".js-text");
+console.log(btnEl);
 
+const title = getEl(".js-title");
+console.log(title);
