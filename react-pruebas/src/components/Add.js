2257ac2db6e78ba105d1a22e143e4cf5d045ddaa
Lorena <lorena.perez@gmail.com>
2022-01-04 12:18:52 +0100

    change calculator

--- react-pruebas/src/components/Add.js
+++ react-pruebas/src/components/Add.js
@@ -3,6 +3,8 @@ import { useState } from "react";
 const Add = () => {
   const [numberA, setNumberA] = useState(0);
   const [numberB, setNumberB] = useState(0);
+  const [operation, setOperation] = useState("Sumar");
+  const [reseat, setReseat] = useState("");
 
   const handleNumberA = (ev) => {
     setNumberA(ev.target.value);
@@ -10,12 +12,27 @@ const Add = () => {
   const handleNumberB = (ev) => {
     setNumberB(ev.target.value);
   };
-  //   const total = parseInt(numberA) + parseInt(numberB);
+  const handleOperation = (ev) => {
+    setOperation(ev.target.value);
+  };
+  const handleReseat = (ev) => {
+    SetReseat();
+  };
+  const result = parseInt(numberA) + parseInt(numberB);
 
   return (
     <div>
       <h1 className="title">La calculadora</h1>
       <form>
+        <label className="form__label">¿Qué operación quieres hacer?</label>
+        <select name="operation" id="operation" onChange={handleOperation}>
+          Suma
+          <option value="+">Sumar</option>
+          <option value="-">Restar</option>
+          <option value="*">Multiplicar</option>
+          <option value="/">Dividir</option>
+        </select>
+        <br />
         <label className="form__label">
           Escriba un número:
           <input
@@ -24,19 +41,16 @@ const Add = () => {
             className="form__input"
           />
         </label>
-        <label className="form__label">
-          Escriba otro número:
-          <input
-            onChange={handleNumberB}
-            type="number"
-            className="form__input"
-          />
-        </label>
-        <p>
-          La suma de {numberA}y {numberB} es{" "}
-          {parseInt(numberA) + parseInt(numberB)}
-        </p>
+        <br />
+        <label className="form__label"></label>
+        Escriba otro número:
+        <input onChange={handleNumberB} type="number" className="form__input" />
       </form>
+      <p>
+        La {operation} de <strong>{numberA}</strong> y{" "}
+        <strong>{numberB}</strong> es <strong>{result}</strong>.
+      </p>
+      <button onClick={handleReseat}>Reset</button>
     </div>
   );
 };
