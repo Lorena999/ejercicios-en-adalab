f4f845c5ec41921f2b3749fb5c7b92c8422baa5b
Lorena <lorena.perez@gmail.com>
2022-01-07 15:27:42 +0100

    add localstorage

--- use-effect/src/components/App.js
+++ use-effect/src/components/App.js
@@ -1,10 +1,12 @@
 import "../styles/App.css";
+import LocalStorage from "./LocalStorage";
 import Paranormal from "./Paranormal";
 import Shows from "./Shows";
 
 function App() {
   return (
     <div className="App">
+      <LocalStorage />
       <Shows />
       <Paranormal />
     </div>
