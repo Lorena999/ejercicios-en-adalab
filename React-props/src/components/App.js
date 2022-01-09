8077eab6f155702a3858497ed21030d6cbb9b07c
Lorena <lorena.perez@gmail.com>
2022-01-09 13:15:37 +0100

    add exercise props

---
+++ React-props/src/components/App.js
@@ -0,0 +1,11 @@
+import "../styles/App.css";
+
+function App() {
+  return (
+    <div classNameName="App">
+      <h1 className="header__title">Hola mundo</h1>
+    </div>
+  );
+}
+
+export default App;
