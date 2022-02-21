671669930e9b48bb95bdc9a6cf3c19ed0e5c9f18
lorena <lorena@MacBook-Pro-de-lorena.local>
2022-02-21 11:45:18 +0100

    add change 2.1

--- Modulo 2/2-3/Exercise-1/main.js
+++ Modulo 2/2-3/Exercise-1/main.js
@@ -1,13 +1,13 @@
 "use strict";
+const userImg = document.querySelector(".user__avatar");
 
 // avatar por defecto
 const DEFAULT_AVATAR = "http://placehold.it/300x300";
 // avatar que eligió el usuario al registrarse
-let userAvatar = "http://www.fillmurray.com/300/300";
+let userAvatar = "";
 
 //userAvatar = 'http://www.fillmurray.com/300/300';
-const userImg = document.querySelector(".user__avatar");
-userImg.src = userAvatar;
-//userImg.src =  `${userAvatar || DEFAULT_AVATAR}`;
 
+//userImg.src =  `${userAvatar || DEFAULT_AVATAR}`;
 userImg.src = `${userAvatar || DEFAULT_AVATAR}`;
+// userImg.src = `${userAvatar || DEFAULT_AVATAR}`;
